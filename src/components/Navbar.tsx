b8bf59af8a3e9ac9f45d59e57ee52c09fc1e0cf0
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-10-21 11:31:27 -0500

    creando componente barra de navegacion

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-blue-600 text-white px-6 py-3 flex gap-6">
+      <Link href="/">Inicio</Link>
+      <Link href="/implementos">Implementos</Link>
+      <Link href="/entrenadores">Entrenadores</Link>
+      <Link href="/usuarios">Usuarios</Link>
+    </nav>
+  );
+}
