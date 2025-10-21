817e7d0d5e5fb0ad5862a1c43d4800c5b88ace5b
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-10-21 11:34:33 -0500

    actualizando layout.tsx and page.tsx

--- src/components/Bienvenida.tsx
+++ src/components/Bienvenida.tsx
@@ -9,7 +9,7 @@ export default function Bienvenida({
   return (
     <main className="flex flex-col justify-center items-center p-4 bg-amber-200">
       <h1 className="text-2xl font-semibold mb-1">!Bienvenido, {nombre}</h1>
-      <p className="text-blue-500">GymCode in Next.js + Backend en Java</p>
+      <p className="text-blue-500">GymTech</p>
     </main>
   );
 }
